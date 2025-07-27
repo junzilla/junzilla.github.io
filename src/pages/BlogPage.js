3670962d48734403f2e01a9365cf6d9778503405
junzilla <771617640abc@gmail.com>
2025-07-27 06:26:04 -0400

    Add drawer on BlogPage

--- src/pages/BlogPage.js
+++ src/pages/BlogPage.js
@@ -5,7 +5,7 @@ import BlogDetail from "../components/BlogDetail";
 import BlogWallPaper from '../components/BlogWallPaper';
 import MusicNoteIcon from '@mui/icons-material/MusicNote';
 import MusicPlayerSlider from "../components/MusicPlayer";
-import { Box, Typography, IconButton } from "@mui/material";
+import { Box, Typography, IconButton, Drawer } from "@mui/material";
 import useMediaQuery from '@mui/material/useMediaQuery';
 import {
     Timeline,
@@ -16,6 +16,7 @@ import {
     TimelineDot,
 } from '@mui/lab';
 import { useTheme } from "@mui/material/styles";
+import LinearScaleIcon from '@mui/icons-material/LinearScale';
 
 
 const BlogPage = () => {
@@ -25,46 +26,64 @@ const BlogPage = () => {
     const isMobile = useMediaQuery(theme.breakpoints.down("sm")); // 小于600px 为 mobile
     const [showPlayer, setShowPlayer] = useState(!isMobile);
 
+    const [drawerOpen, setDrawerOpen] = useState(false);
+
+
+    const drawerContent = (
+        <Box sx={{ width: 200, borderRight: 3, borderColor: "divider", mt: 8 }}>
+            <Timeline position="alternate">
+                {sortedBlogs.map((blog, index) => {
+                    const isSelected = selectedBlog.id === blog.id;
+                    return (
+                        <TimelineItem key={blog.id}>
+                            <TimelineSeparator>
+                                <TimelineDot
+                                    sx={{
+                                        backgroundColor: isSelected ? 'primary.main' : 'white',
+                                    }}
+                                />
+                                {index < sortedBlogs.length - 1 && (
+                                    <TimelineConnector sx={{ backgroundColor: 'white' }} />
+                                )}
+                            </TimelineSeparator>
+                            <TimelineContent>
+                                <Typography
+                                    onClick={() => setSelectedBlog(blog)}
+                                    sx={{
+                                        cursor: 'pointer',
+                                        color: isSelected ? 'primary.main' : 'text.primary',
+                                        fontWeight: isSelected ? 'bold' : 'normal',
+                                    }}
+                                >
+                                    {blog.date}
+                                </Typography>
+                            </TimelineContent>
+                        </TimelineItem>
+                    );
+                })}
+            </Timeline>
+        </Box>
+    )
+
+
 
     return (
         <Box sx={{ display: "fixed", height: "100%" }}>
             {/* 左侧时间轴 */}
-            <Box sx={{ width: 200, borderRight: 3, borderColor: "divider", mt: 8 }}>
-                <Timeline position="alternate">
-                    {sortedBlogs.map((blog, index) => {
-                        const isSelected = selectedBlog.id === blog.id;
-                        return (
-                            <TimelineItem key={blog.id}>
-                                <TimelineSeparator>
-                                    <TimelineDot
-                                        sx={{
-                                            backgroundColor: isSelected ? 'primary.main' : 'white',
-                                        }}
-                                    />
-                                    {index < sortedBlogs.length - 1 && (
-                                        <TimelineConnector sx={{ backgroundColor: 'white' }} />
-                                    )}
-                                </TimelineSeparator>
-                                <TimelineContent>
-                                    <Typography
-                                        onClick={() => setSelectedBlog(blog)}
-                                        sx={{
-                                            cursor: 'pointer',
-                                            color: isSelected ? 'primary.main' : 'text.primary',
-                                            fontWeight: isSelected ? 'bold' : 'normal',
-                                        }}
-                                    >
-                                        {blog.date}
-                                    </Typography>
-                                </TimelineContent>
-                            </TimelineItem>
-                        );
-                    })}
-                </Timeline>
-            </Box>
+            {!isMobile && (
+                <Box sx={{ width: 200, borderRight: 3, borderColor: "divider", mt: 8 }}>
+                    {drawerContent}
+                </Box>
+            )}
 
             {/* 右侧Blog细节 */}
             <Box sx={{ flexGrow: 1, paddingLeft: 3, paddingRight: 3, paddingBottom: 3, mt: 9 }}>
+                {/* 移动端菜单按钮 */}
+                {isMobile && (
+                    <IconButton onClick={() => setDrawerOpen(true)} sx={{ ml: -1, color: "#ffffff" }} >
+                        <LinearScaleIcon sx={{ transform: 'rotate(90deg)' }} />
+                    </IconButton>
+                )}
                 <BlogDetail blog={selectedBlog} />
                 <BlogWallPaper />
             </Box>
@@ -82,22 +101,39 @@ const BlogPage = () => {
                         }
                     }}
                 >
-                    <MusicNoteIcon fontSize="small" />
+                    <MusicNoteIcon />
                 </IconButton>
             </Box>
 
             {/* If MusicNoteIcon == 1, show Player */}
-                <Box
-                    sx={{
-                        display: showPlayer ? 'block' : 'none',
-                        position: 'fixed',
-                        top: '50%',
-                        left: '50%',
-                        transform: 'translate(-50%, -50%)',
-                        zIndex: 20
-                    }}>
-                    <MusicPlayerSlider />
-                </Box>
+            <Box
+                sx={{
+                    display: showPlayer ? 'block' : 'none',
+                    position: 'fixed',
+                    top: '50%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)',
+                    zIndex: 20
+                }}>
+                <MusicPlayerSlider />
+            </Box>
+            {/* 移动端 Drawer */}
+            <Drawer
+                anchor="left"
+                open={drawerOpen}
+                onClose={() => setDrawerOpen(false)}
+                ModalProps={{ keepMounted: true }}
+                slotProps={{
+                    paper: {
+                        sx: {
+                            top: '65px',
+                            height: 'calc(100% - 65px)',
+                        },
+                    },
+                }}
+            >
+                {drawerContent}
+            </Drawer>
         </Box>
     );
 };
